910d0faae0a9b933476ee67ee7e953f5f834788a
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2024-12-29 20:40:29 +0600

    Create 490A - Team Olympiad.cpp

---
+++ 490A - Team Olympiad.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t, n;
+    vector<int> a, b, c;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        cin >> n;
+        if (n == 1) {
+            a.push_back(i + 1);
+        } else if (n == 2) {
+            b.push_back(i + 1);
+        } else if (n == 3) {
+            c.push_back(i + 1);
+        }
+    }
+
+    int w = min({(int)a.size(), (int)b.size(), (int)c.size()});
+
+    if (w > 0) {
+        cout << w << endl;
+        for (int i = 0; i < w; i++) {
+            cout << a[i] << " " << b[i] << " " << c[i] << endl;
+        }
+    } else {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
