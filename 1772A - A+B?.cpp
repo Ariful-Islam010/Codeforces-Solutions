acb4aa72428c1b66e9c92d71bf09698af8b74dce
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2024-12-31 13:48:47 +0600

    Create 1772A - A+B?.cpp

---
+++ 1772A - A+B?.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t,a,b;
+    char c;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>c>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
