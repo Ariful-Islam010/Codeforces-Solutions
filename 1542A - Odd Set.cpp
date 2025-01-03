f90ef1ffc12c5e8e8d1bc3f4d592a707feea052c
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-01-04 01:10:42 +0600

    Create 1542A - Odd Set.cpp

---
+++ 1542A - Odd Set.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        int even_count = 0, odd_count = 0;
+        for (int i = 0; i < 2 * n; i++) {
+            int a;
+            cin >> a;
+            if (a % 2 == 0) {
+                even_count++;
+            } else {
+                odd_count++;
+            }
+        }
+        
+        if (even_count == odd_count) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
