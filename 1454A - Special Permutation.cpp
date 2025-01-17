2e999c3606903d5af91f5b9f095745b14c61a3ac
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-01-17 18:51:41 +0600

    Create 1454A - Special Permutation.cpp

---
+++ 1454A - Special Permutation.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ll i,t,n,x;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        ll x=n;
+        for(i=2;i<=n;i++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<1<<endl;
+    }
+    return 0;
+}
